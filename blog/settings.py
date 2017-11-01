95ad110bc1cbfb51b6e605d3f176e9e32cd2502e
Jensonss <songjlforever@foxmail.com>
2017-11-01 12:13:13 +0800

    allowed_host修改为*

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '^$x^aisl@uoi$m$5c@4$h2m=gx++22k&p%v_u8f@&67-v@cq#h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
